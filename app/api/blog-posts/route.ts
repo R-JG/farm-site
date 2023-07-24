8114fa7c746df4283d2b312eb3365077b2e22c5b
Robin Guerrier <robin.james.guerrier@gmail.com>
2023-07-24 02:29:49 -0400

    Remove admin content api test

--- app/api/blog-posts/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-import { getAllBlogPosts } from '@/lib/database';
-
-export const GET = async () => {
-  const allBlogPosts = await getAllBlogPosts();
-  return NextResponse.json(allBlogPosts);
-};
