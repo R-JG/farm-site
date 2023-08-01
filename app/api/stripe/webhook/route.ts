ae4e71dd14e6c3ef290cd0ebbfaf3310df10d54e
Robin Guerrier <robin.james.guerrier@gmail.com>
2023-08-01 01:27:20 -0400

    Add db shop item inventory update to webhook

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -1,20 +1,30 @@
 import { NextRequest, NextResponse } from 'next/server';
 import stripe from '@/utils/stripe';
+import { prisma } from '@/prisma/database';
 
 export const POST = async (request: NextRequest): Promise<Response> => {
   try {
     const event = await request.json();
     if (event.type === 'checkout.session.completed') {
       const checkoutSessionCompleted = event.data.object;
-      if (typeof checkoutSessionCompleted.id !== 'string') {
-        return new NextResponse('', { status: 400 });
-      };
       const expandedSession = await stripe.checkout.sessions.retrieve(
         checkoutSessionCompleted.id, { expand: ['line_items'] }
       );
-      console.log('LINEITEMS --> ', expandedSession.line_items);
-    };
+      if (expandedSession.line_items) {
+        const inventoryUpdates = await Promise.all(expandedSession.line_items.data.map(async lineItem => {
+          if (!lineItem.quantity || !lineItem.price || 
+          (typeof lineItem.price.product !== 'string')) return;
 
+          console.log('PRODUCT ID --> ', lineItem.price.product);
+          console.log('PRODUCT QUANTITY --> ', lineItem.quantity);
+
+          return prisma.shopItem.update({ 
+            where: { id: lineItem.price.product, inventory: { not: null } }, 
+            data: { inventory: { decrement: lineItem.quantity } } });
+        }));
+        console.log('The following inventory updates have been applied: ', inventoryUpdates);
+      };
+    };
     return new NextResponse('', { status: 200 });
   } catch (error) {
     console.error(error);
