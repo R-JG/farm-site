94b71e019f0e1949f5cb446e2c79b623cd3e890d
Robin Guerrier <robin.james.guerrier@gmail.com>
2023-08-06 22:02:25 -0400

    Add newsletter form functionality

--- components/NewsletterForm.tsx
+++ components/NewsletterForm.tsx
@@ -2,37 +2,52 @@
 
 import { useState, ChangeEvent, FormEvent } from 'react';
 
-const NewsletterForm = () => {
+type Props = {
+  addNewsletterSubscriber: (email: string) => Promise<void>
+};
 
+const NewsletterForm = ({ addNewsletterSubscriber }: Props) => {
+
+  const [buttonText, setButtonText] = useState<'Subscribe' | 'Thanks for subscribing!'>('Subscribe');
   const [emailInputValue, setEmailInputValue] = useState('');
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
     setEmailInputValue(e.currentTarget.value);
   };
 
-  const handleFormSubmit = (e: FormEvent<HTMLFormElement>): void => {
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
-
+    if (emailInputValue === '') return;
+    try {
+      addNewsletterSubscriber(emailInputValue);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setEmailInputValue('');
+      setButtonText('Thanks for subscribing!');
+    };
   };
 
   return (
     <form 
       onSubmit={handleFormSubmit}
-      className=''
+      className='min-w-[26rem] flex flex-col'
     >
-      <h1 className='mb-1'>
+      <span className='mb-1'>
         Subscribe to our newsletter!
-      </h1>
-      <input
-        type='email'
-        placeholder='Email address' 
-        value={emailInputValue}
-        onChange={handleInputChange}
-        className='p-1 mr-3 border-blue-100 border-2 rounded'
-      />
-      <button className='p-2 bg-blue-200 rounded active:bg-blue-300 hover:scale-105 transition-all'>
-        Subscribe
-      </button>
+      </span>
+      <div className='flex flex-row justify-start items-center'>
+        <input
+          type='email'
+          placeholder='Email address' 
+          value={emailInputValue}
+          onChange={handleInputChange}
+          className='p-1 mr-3 border-blue-100 border-2 rounded'
+        />
+        <button className='p-2 bg-blue-200 rounded active:bg-blue-300 hover:scale-105 transition-all'>
+          {buttonText}
+        </button>
+      </div>
     </form>
   );
 };
