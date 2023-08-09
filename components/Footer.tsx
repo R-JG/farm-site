11ca58db91f65af36d2ff27df0ec4e12b0bb6b11
Robin Guerrier <robin.james.guerrier@gmail.com>
2023-08-09 02:55:32 -0400

    Change next version to canary

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import NewsletterForm from './NewsletterForm';
 import IconLink from './IconLink';
-import EFAOMemberLogo from '@/public/EFAO-member-logo.png';
+import efaoMemberLogo from '@/public/efao-member-logo.png';
 import facebookIcon from '@/public/icon--facebook.svg';
 import instagramIcon from '@/public/icon--instagram.svg';
 import { MAILERLITE_API_KEY } from '@/utils/config';
@@ -32,7 +32,7 @@ const Footer = () => {
   return (
     <footer className='w-full h-fit pt-10 pb-7 bg-blue-50 flex flex-row justify-evenly items-start'>
       <Image 
-        src={EFAOMemberLogo}
+        src={efaoMemberLogo}
         alt='EFAO member'
         className=' w-28 aspect-square'
       />
